926dfa1fd3b93291199d58fea19a03cd532beb21
fcocc77 <fcocc77@gmail.com>
2018-10-09 11:01:06 -0300

    fix: chkconfig para los servicios

--- setup.py
+++ setup.py
@@ -236,8 +236,12 @@ def linux_install():
 	sh( "sed -i -e 's/\r//g' /etc/init.d/cserver")
 	sh( "sed -i -e 's/\r//g' /etc/init.d/cmanager")  
 	#--------------------------------------------------------------------------------
-	if server_start: os.system( "service cserver start" )
-	if manager_start: os.system( "service cmanager start " )
+	if server_start: 
+		os.system( "service cserver start" )
+		os.system( "chkconfig cserver on" )
+	if manager_start: 
+		os.system( "service cmanager start " )
+		os.system( "chkconfig cmanager on" )
 
 	nuke_module(1)
 
