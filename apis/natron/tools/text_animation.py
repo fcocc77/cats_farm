7bb7b0e0b7241dfdcc2caae31c307938332e23df
Francisco <fcocc77@gmail.com>
2020-03-10 18:30:04 -0300

    natron: text animacion avance...

--- apis/natron/tools/text_animation.py
+++ apis/natron/tools/text_animation.py
@@ -1,44 +1,97 @@
 app = app1
 
 
-text_group = app.TextAnimation
+created_nodes = []
 
 
-def create_letter(letter, position):
+def createNode(name, group=app):
+    nodes = {
+        'blur': 'net.sf.cimg.CImgBlur',
+        'text': 'net.fxarena.openfx.Text',
+        'transform': 'net.sf.openfx.TransformPlugin',
+        'merge': 'net.sf.openfx.MergePlugin'
+    }
 
-    text_name = app.createNode(
-        "net.fxarena.openfx.Text", -1, text_group).getScriptName()
-    transform_name = app.createNode(
-        "net.sf.openfx.TransformPlugin", -1, text_group).getScriptName()
+    node_name = app.createNode(nodes[name], -1, group).getScriptName()
 
-    text = getattr(text_group, text_name)
+    node = getattr(group, node_name)
+    created_nodes.append(node)
+    return node
+
+
+def deleteNodes():
+    app.setSelection(created_nodes)
+
+
+def create_letter(letter, position, index):
+
+    def expression(field, name, index, dimension=0, add=0):
+        exp = 'value = thisGroup.' + name + \
+            '.curve(frame - ' + str(index) + '*thisGroup.delay.get());'
+        exp += 'ret = value + ' + str(add)
+
+        field.setExpression(exp, False, dimension)
+
+    # create text
+    text = createNode('text', textAnimation)
     text.autoSize.set(True)
     text.text.set(letter)
+    # Opacity expression
+    for i in range(3):
+        expression(text.color, 'opacity', index, i)
+    # ------------------------
+
+    # Blur
+    blur = createNode('blur', textAnimation)
+    blur.cropToFormat.set(False)
+    blur.connectInput(0, text)
+    expression(blur.size, 'blur_x', index, 0)
+    expression(blur.size, 'blur_y', index, 1)
+    # ------------------
+
     letter_width = text.getRegionOfDefinition(1, 1).x2
 
-    transform = getattr(text_group, transform_name)
-    transform.translate.set(position, 0)
+    transform = createNode('transform', textAnimation)
 
-    transform.connectInput(0, text)
+    # Transform expression
+    expression(transform.translate, 'position_x', index, 0, position)
+    expression(transform.translate, 'position_y', index, 1)
+    expression(transform.rotate, 'rotate', index)
+    expression(transform.scale, 'scale', index, 0)
+    expression(transform.scale, 'scale', index, 1)
+    # -----------------------
+
+    transform.connectInput(0, blur)
 
     return [transform, letter_width]
 
 
-merge = app.createNode('net.sf.openfx.MergePlugin', -1, text_group)
-text_group.Output1.connectInput(0, merge)
+def create_word():
+    merge = createNode('merge', textAnimation)
+    textAnimation.Output3.connectInput(0, merge)
+
+    pos = 0
+    word = textAnimation.text.get()
+    for index, letter in enumerate(word):
+        transform, letter_width = create_letter(letter.strip(), pos, index)
+
+        # la entrada numero 2 pertenece a la maskara, asi que la omite
+        if index >= 2:
+            index += 1
+
+        merge.connectInput(index, transform)
+        pos += letter_width
 
 
-word = 'Francisco'
+# node de text animacion
+def update_callback(thisParam, thisNode, thisGroup, app, userEdited):
+    button_name = thisParam.getScriptName()
+    if button_name == 'update':
+        create_word()
+    if button_name == 'delete':
+        deleteNodes()
 
 
-pos = 0
-
-for index, letter in enumerate(word):
-    transform, letter_width = create_letter(letter.strip(), pos)
-
-    # la entrada numero 2 pertenece a la maskara, asi que la omite
-    if index >= 2:
-        index += 1
-
-    merge.connectInput(index, transform)
-    pos += letter_width
+textAnimation = app.TextAnimation
+textAnimation.onParamChanged.set('update_callback')
+# -----------------------------
