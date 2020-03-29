15f30ab34693026dc5840c9fc28e4286b48c301c
Francisco <fcocc77@gmail.com>
2020-03-29 15:00:25 -0300

    ...

--- apis/natron/tools/core/init.py
+++ apis/natron/tools/core/init.py
@@ -1,26 +1,27 @@
 import os
+import shutil
+import filecmp
 import NatronEngine
 
 
 def reload_nodes():
+    develop_plugins = '/home/pancho/Documents/GitHub/vinarender/apis/natron/tools/plugins'
+    natron_plugins = '/opt/Natron2/Plugins/PyPlugs'
+
     # recarga todos los nodos, de videovina, con el comando 'Control-R'
     # para poder desarrollar los plugins sin recargar el natron.
+    for plugin in os.listdir(develop_plugins):
+        develop_plugin = develop_plugins + '/' + plugin
+        natron_plugin = natron_plugins + '/' + plugin
 
-    natron_tools = '/home/pancho/Documents/GitHub/vinarender/apis/natron/tools'
-    plugins = natron_tools + '/plugins'
+        # si el plugin no es el mismo lo copia a la
+        # carpeta de natron y lo recarga
+        if not filecmp.cmp(develop_plugin, natron_plugin):
+            shutil.copy(develop_plugin, natron_plugin)
 
-    # reinstala los nodos
-    os.system('cd ' + natron_tools + ' && sh install.sh')
-    # ---------------------
-
-    # recarga todos los plugin que estan en la carpeta plugins
-    for plugin in os.listdir(plugins):
-        plugin = plugin.split('.')[0]
-
-        reload(eval(plugin))
-    # -------------------------
-
-    print('Reload OK')
+            plugin = plugin.split('.')[0]
+            reload(eval(plugin))
+            print('Reload OK')
 
 
 NatronGui.natron.addMenuCommand('videovina/Reload Nodes', 'reload_nodes',
