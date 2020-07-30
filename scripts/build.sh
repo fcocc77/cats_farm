7280d64784025d582fff24ada369186cc19049dd
Francisco Contreras <fcocc77@gmail.com>
2020-07-30 12:53:15 -0400

    build correccion y eliminacion de win.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,14 @@
 thread=4
 
-pkill -9 "vmonitor"
-
 qmake-qt5
 make -j $thread
 
-src/monitor/release/vmonitor
+rm ./bin/*
+
+# copia los ejecutables a la carpeta bin
+cp ./src/manager/release/vmanager ./bin
+cp ./src/server/release/vserver ./bin
+cp ./src/monitor/release/vmonitor ./bin
+cp ./src/logger/release/logger ./bin
+cp ./src/submit/release/submit ./bin
+cp ./src/videovina/release/videovina ./bin
