4a09428d80393725b081b05d20db6241b48904ee
pancho <pancho@localhost.localdomain>
2021-07-11 18:21:03 -0400

    ..

--- services/service.sh
+++ services/service.sh
@@ -6,13 +6,13 @@ log_file=$path'/log/$name crash - '$(date)'.log'
 debug=false
 
 run() {
-	# mata el pid a partir del puerto de vinarender
-	fuser -k $port/tcp &>/dev/null
+    # mata el pid a partir del puerto de vinarender
+    fuser -k $port/tcp &>/dev/null
 
-	if $debug; then
-		rm $log_file &>/dev/null
-		echo "Started debugging."
-		gdb -ex "set logging file $log_file" \
+    if $debug; then
+        rm $log_file &>/dev/null
+        echo "Started debugging."
+        gdb -ex "set logging file $log_file" \
             -ex "set logging redirect on" \
             -ex "set confirm off" \
             -ex "set pagination off" \
@@ -20,39 +20,39 @@ run() {
             -ex "set logging on" \
             -ex bt \
             -ex q "./$name" &>/dev/null
-	else
-		"./$name" &>/dev/null
-	fi
+    else
+        "./$name" &>/dev/null
+    fi
 }
 
 start() {
-	pscax=$(ps cax | grep $name)
+    pscax=$(ps cax | grep $name)
 
-	if [[ $pscax != *"$name"* ]]; then
-		echo "VinaRender $name has started."
-		export DISPLAY=:1 && export QT_QPA_PLATFORM=offscreen && cd $path'/bin' && run &>/dev/null &
-	else
-		echo "VinaRender $name is running now."
-	fi
+    if [[ $pscax != *"$name"* ]]; then
+        echo "VinaRender $name has started."
+        export DISPLAY=:1 && export QT_QPA_PLATFORM=offscreen && cd $path'/bin' && run &>/dev/null &
+    else
+        echo "VinaRender $name is running now."
+    fi
 }
 
 stop() {
-	pscax=$(ps cax | grep $name)
+    pscax=$(ps cax | grep $name)
 
-	if [[ $pscax == *"$name"* ]]; then
-		pkill -9 $name
-		echo "VinaRender $name has stopped."
-	else
-		echo "Is not running."
-	fi
+    if [[ $pscax == *"$name"* ]]; then
+        pkill -9 $name
+        echo "VinaRender $name has stopped."
+    else
+        echo "Is not running."
+    fi
 }
 
 if [ "$1" = "start" ]; then
-	start
+    start
 fi
 
 if [ "$1" = "stop" ]; then
-	stop
+    stop
 fi
 
 exit 0
