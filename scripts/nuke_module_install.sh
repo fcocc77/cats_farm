623686c29b7085593d9d12cca37b45729016c362
Francisco <fcocc77@gmail.com>
2021-12-18 13:04:34 -0300

    nuke_module_install: multi version ...

--- scripts/nuke_module_install.sh
+++ scripts/nuke_module_install.sh
@@ -1,24 +1,35 @@
 
-nuke_plugins='/opt/Nuke12.2v9/plugins'
-menu_py="$nuke_plugins/menu.py"
-
 nuke_install()
 {
     cd $1
 
-    echo 'import nuke_vinarender #vinarender' >> $menu_py
-    echo 'nuke.menu("Nuke").addCommand("Vina/VinaRender Submit", "nuke_vinarender.submit()", "Ctrl+R") #vinarender' >> $menu_py
+    for version in $(ls '/opt' | grep Nuke); do
 
-    cp './modules/nuke_vinarender.py' $nuke_plugins
-    cp './python_util/util.py' "$nuke_plugins/vinarender_util.py"
+        nuke_plugins="/opt/$version/plugins"
+        menu_py="$nuke_plugins/menu.py"
+
+        echo 'import nuke_vinarender #vinarender' >> $menu_py
+        echo 'nuke.menu("Nuke").addCommand("Vina/VinaRender Submit", "nuke_vinarender.submit()", "Ctrl+R") #vinarender' >> $menu_py
+
+        cp './modules/nuke_vinarender.py' $nuke_plugins
+        cp './python_util/util.py' "$nuke_plugins/vinarender_util.py"
+
+    done
 }
 
 nuke_uninstall()
 {
-    sed -i '/#vinarender/d' $menu_py
+    for version in $(ls '/opt' | grep Nuke); do
 
-    rm "$nuke_plugins/nuke_vinarender.py"
-    rm "$nuke_plugins/nuke_vinarender.pyc"
-    rm "$nuke_plugins/vinarender_util.py"
-    rm "$nuke_plugins/vinarender_util.pyc"
+        nuke_plugins="/opt/$version/plugins"
+        menu_py="$nuke_plugins/menu.py"
+
+        sed -i '/#vinarender/d' $menu_py
+
+        rm "$nuke_plugins/nuke_vinarender.py"
+        rm "$nuke_plugins/nuke_vinarender.pyc"
+        rm "$nuke_plugins/vinarender_util.py"
+        rm "$nuke_plugins/vinarender_util.pyc"
+
+    done
 }
